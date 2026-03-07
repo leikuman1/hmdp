f2ef7ee701ece9637a184578bbbede99d57d100b
leikuman1 <2775049706@qq.com>
2026-03-07 16:05:12 +0800

    查询商铺种类缓存业务代码开发

--- src/main/java/com/hmdp/service/IShopTypeService.java
+++ src/main/java/com/hmdp/service/IShopTypeService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopTypeService extends IService<ShopType> {
 
+    /**
+     * 查询各个类型的店铺
+     * @return
+     */
+    Result queryShopTypeList();
 }
